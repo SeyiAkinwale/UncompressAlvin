ed8c01a52b291b4960ec05fb9719f06d831a55e5
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-03-06 18:19:26 +0000

    while loop

--- .ccls-cache/@home@runner@UncompressAlvin/main.cpp
+++ .ccls-cache/@home@runner@UncompressAlvin/main.cpp
@@ -6,6 +6,13 @@ string uncompress(string s)
 {
   string result = "";
   string numbers = "0123456789";
+  int i = 0, j = 0;
+  while (j<s.length())
+    {
+
+      j++;
+      i = j;
+    }
   return result;
 }
 
